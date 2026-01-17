6d02e515e22658fcb99f00d310198a94a121614a
Ravikiran Bathe <ravikiranbathe2005@gmail.com>
2026-01-17 20:32:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2635270578-749bb",
+  "appId": "1:385882673808:web:23deeb803c6f13ae09fe15",
+  "apiKey": "AIzaSyC6i22O_4RhYnMgP06ptzQlmXFw68CXO_A",
+  "authDomain": "studio-2635270578-749bb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "385882673808"
+};
